d45914076763df0889386cd6a43a18e2ee5195e3
Alexandros Soulis <alexsoulis58@gmail.com>
2023-12-11 20:34:41 +0200

    Main updated I

--- src/gr/aueb/cf/proj5/Main.java
+++ src/gr/aueb/cf/proj5/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Coding Factory!!!!!!");
     }
+
+    public static void HelloWorld() {
+        System.out.println("Hello Coding Factory !!!");
+    }
 }
